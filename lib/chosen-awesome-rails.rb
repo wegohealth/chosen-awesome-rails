73b2e23ce453433dc5d657afcc8abe43a6a92ec6
HassanAkbar <hassanakbar2@gmail.com>
2021-12-22 17:00:25 +0500

    Support Rails 6 and 7

--- lib/chosen-awesome-rails.rb
+++ lib/chosen-awesome-rails.rb
@@ -6,7 +6,7 @@ end
 require 'chosen-awesome-rails/version'
 
 case ::Rails.version.to_s
-  when /^4|5/
+  when /^4|5|6|7/
     require 'chosen-awesome-rails/engine'
   when /^3\.[12]/
     require 'chosen-awesome-rails/engine3'
